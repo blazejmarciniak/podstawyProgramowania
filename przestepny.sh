21ed962de95f9dc1ab2d746b0ba1be5e0eb34a93
Blazej Marciniak <blazej.marciniak@biol.uni.lodz.pl>
2025-11-18 23:15:15 +0100

    check conditions added

--- przestepny.sh
+++ przestepny.sh
@@ -1,3 +1,24 @@
 #!/user/bin/bash
 read -p "podaj rok: " rok
 
+if (( $rok%400==0 )); then
+	echo "$rok jest przestepny"
+else
+	if (( $rok%4==0 )); then
+		if(($rok%100)); then
+			echo "$rok jest przestepny"
+		else
+			echo "$rok nie jest przestepny"
+		fi
+		
+	else
+		echo "$rok nie jest przestepny"
+	fi
+fi
+
+if (($rok%400==0)) || (($rok%4==0 && $rok%100!=0));then
+	echo "przestepny"
+else
+	echo "nie jest przestepny"
+fi
+

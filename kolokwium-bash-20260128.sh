90d3e0b49c7495006451b2b9b0103ac9ac74293c
Blazej Marciniak <blazej.marciniak@biol.uni.lodz.pl>
2026-01-28 08:44:19 +0100

    kolokiwum poprawkowe

---
+++ kolokwium-bash-20260128.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+#Wypisz wszystkie liczby podzielne przez 100 i niepodzielne przez 200 lub 400 z przedziału 75 – 1512
+
+#Korzystając z pętli while wyświetl co drugą liczbę podzielną przez 6 z przedziału podanego przez użytkownika. 
+
+#Zadeklaruj dwie tablice równych rozmiarów. Następnie wykonaj sortowanie pierwszej rosnąco drugiej malejąco. Wyświetl obie tablice przed rozpoczęciem sortowania i po jego zakończeniu, zweryfikuj czy zostały poprawnie posortowane.
+
+#Korzystając z tablic z poprzedniego zadania wykonaj sumowanie wszystkich elementów zgodnie z kluczem największy z pierwszej tablicy z największym z drugiej, poprzednik największego z pierwszej tablicy z następnikiem największego drugiej tablicy, itd.
+
